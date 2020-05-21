61592683bee0b66abfd6c5083208e53299571ab3
Oleg <fasfas7971@gmail.com>
2020-05-21 12:12:29 +0300

    Updates

---
+++ precache-manifest.97b8954813d42fe12d87428e97715922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd0843dde79f37689da50234905240c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "06ebea594059bbba94b5",
+    "url": "./static/css/main.2d7a65b8.chunk.css"
+  },
+  {
+    "revision": "95e2ac7a91498eac1241",
+    "url": "./static/js/2.49182981.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.49182981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06ebea594059bbba94b5",
+    "url": "./static/js/main.fa7c76ed.chunk.js"
+  },
+  {
+    "revision": "548aac5e4720dc7e1ea8",
+    "url": "./static/js/runtime-main.9c94e0be.js"
+  }
+]);
